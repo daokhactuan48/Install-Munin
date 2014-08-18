f70c6cdb961c2ae2b65f395b092d301cedf9645e
daokhactuan48 <khactuank55@yahoo.com>
2014-08-17 21:19:27 -0700

    Create munin-client.sh

---
+++ munin-client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+# Shell cai dat munin cho cac node (cac client)
+
+echo "Bat dau cai dat munin tren cac may client"
+sudo apt-get update
+sudo apt-get -y install munin-node munin-plugins-extra
+
+echo "Da cai xong MUNIN"
+
+echo "Bat dau chinh sua file /etc/munin/munin-node.conf"
+sleep 5
+
+filemunin=/etc/munin/munin-node.conf
+test -f $filemunin.orig || cp $filemunin $filemunin.orig
+# rm $filemunin
+# touch $filemunin
+sed -i 's/allow ^127.*/allow ^172.\\16.\\69.\\15$/g' /etc/munin/munin-node.conf
+ 
+echo "Khoi dong lai munin"
+sleep 5
+sudo restart munin-node
