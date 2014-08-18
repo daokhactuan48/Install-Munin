4f0f06a3cfd2e24e4f24afb92eedde05cf31605f
daokhactuan48 <khactuank55@yahoo.com>
2014-08-18 00:33:41 -0700

    Update add-client.sh
    update them restart

--- add-client.sh
+++ add-client.sh
@@ -3,5 +3,7 @@ read hostname
 printf "IP: -> "
 read IP
 echo "[$hostname]" >> /etc/munin/munin.conf
-echo -e  "\taddress $IP" >> /etc/munin/munin.conf
-echo -e  "\tuse_node_name yes" >> /etc/munin/munin.conf
+echo -e  "    address $IP" >> /etc/munin/munin.conf
+echo -e  "    use_node_name yes" >> /etc/munin/munin.conf
+echo "---Restart Apache"
+service apache2 restart
